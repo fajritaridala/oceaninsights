b4837e5d3aed47c28b3ba570d12c04e0a8d24e60
fajritaridala <140230431+fajritaridala@users.noreply.github.com>
2024-01-01 12:06:24 +0800

    oceaninsights

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="absolute inset-x-0 top-0 z-50">
+        <nav class="flex items-center justify-between p-6" >
+            <div class="flex">
+                <a href=" " class="-m-1.5 p-1.5 ">
+                    <img src="../img/navbar/logo.png" alt="" class="h-8 w-auto" />
+                </a>
+            </div>
+            
+        </nav>
+    </header>
+</body>
+</html>
