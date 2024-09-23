33e32295e69680544bc239a59c726dcb98582375
Muh Fajri Taridala <fadjritaridala@gmail.com>
2024-09-24 00:58:23 +0800

    update responsive homepage

--- public/js/script.js
+++ public/js/script.js
@@ -10,14 +10,14 @@ window.onscroll = function() {
     }
 }
 
-// hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+// // hamburger
+// const hamburger = document.querySelector('#hamburger');
+// const navMenu = document.querySelector('#nav-menu');
 
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
-});
+// hamburger.addEventListener('click', function() {
+//     hamburger.classList.toggle('hamburger-active');
+//     navMenu.classList.toggle('hidden');
+// });
 
 
 
